d0dd76f4e33ea3c2f1edef5bc1a252a80da1b62c
southkorean <peppi@knou.ac.kr>
2021-05-09 04:23:20 +0900

    * uiPropertyAnimator 사용 샘플 작업중

--- UIPropertyAnimator/ViewController.swift
+++ UIPropertyAnimator/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var planetImageView: UIImageView!
+    
+    let animator = UIViewPropertyAnimator(duration: 1.0, curve: .linear)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureInitialSetup()
+        configureEndAnimations()
     }
 
+    func configureInitialSetup() {
+        planetImageView.alpha = 0.0
+        planetImageView.transform = CGAffineTransform(scaleX: 0.01, y: 0.01)
+    }
+    
+    func configureEndAnimations() {
+        animator.addAnimations {
+            self.planetImageView.alpha = 1.0
+            self.planetImageView.transform = CGAffineTransform(scaleX: 3, y: 3)
+        }
+    }
 
+    @IBAction func valueDidChange(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
+        
+    }
 }
 
